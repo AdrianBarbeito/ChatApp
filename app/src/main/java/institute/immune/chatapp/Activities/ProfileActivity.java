b1666f6cbd2a9f0ea21eb1fad9e1bcc330e72516
daviz2402 <david.martinez.ceb@immune.institute>
2022-02-21 00:36:39 +0100

    profile cambiado

--- app/src/main/java/institute/immune/chatapp/Activities/ProfileActivity.java
+++ app/src/main/java/institute/immune/chatapp/Activities/ProfileActivity.java
@@ -8,13 +8,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import institute.immune.chatapp.Class.MyOpenHelper;
 import institute.immune.chatapp.R;
 
 public class ProfileActivity extends AppCompatActivity {
-    private MenuItem chatMenu, profileMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,32 +26,8 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     private void bindings() {
-        chatMenu = findViewById(R.id.itemChat);
-        profileMenu = findViewById(R.id.itemPerson);
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_chatapp, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        Intent intent = null;
-        switch (item.getItemId()){
-            case R.id.itemChat:
-                intent = new Intent(this, ChatActivity.class);
-                break;
-
-            case R.id.itemPerson:
-                intent = new Intent(this, ProfileActivity.class);
-                break;
-        }
-        startActivity(intent);
-        return super.onOptionsItemSelected(item);
-    }
 
 }
